f879f854e7a368fdf843682cd074ee2a44016785
René Pas <info@joint-creations.nl>
2022-11-01 12:00:56 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const animalButtons = function(){
+
+
+const Lion = document.querySelector("big-five-button1",Lion);
+Lion.addEventListener('click',click,showSelected);
+
+const Leopard = document.querySelector("big-five-button2",Leopard);
+Leopard.addEventListener('click',click,showSelected);
+
+const Elephant = document.querySelector("big-five-button3",Elephant);
+Elephant.addEventListener('click',click,showSelected);
+
+const Rhino = document.querySelector("big-five-button4",Rhino);
+Rhino.addEventListener('click',click,showSelected);
+
+const Buffalo = document.querySelector("big-five-button5",Buffalo);
+Buffalo.addEventListener('click',click,showSelected);
+
+document.querySelectorAll("#spotted-animal-list");
+ const spottedanimalslistitem = document.createElement("li");
+spottedanimalslistitem
+
+const listElement = document.add("#spotted-animals-list");
+listElement
+
+const list = document.querySelectorAll("#spotted-animal-list");
+
+
+for(const button of animalButtons){
+    button.addEventListener('change', showSelected);
+    ul.addEventListener('click',click, ()=>{ul.style.background = "#blue" });
+} 
+function showSelected(e) {
+    console.log(e);
+    if(this.click){
+document.querySelector("#spotted-animals-list").innerHTML = `You selected ${this.click}`;
+list.appendChild(spottedanimalslistitem);
+spottedanimalslistitem.appendChild(listElement);
+listElement.appendChild(li.innerHTML = this.click);
+}
+    }
+};
+
+console.log(animalButtons);
+ console.log("big-five-button1","big-five-button2","big-five-button3","big-five-button4","big-five-button5");
