359d7c2b5ec95eb8af2b5d1e541145811de6be65
René Pas <info@joint-creations.nl>
2022-11-05 22:06:46 +0100

    update html and js
    script lets select a button of big five animals spotted on safari and writes it in a list that can be removed and lectively removed from top

--- script.js
+++ script.js
@@ -1,45 +1,73 @@
-const animalButtons = function(){
+// buttens met eventlistener en print functie met log
+const Lion = document.querySelector(".big-five-button1");
+ Lion.addEventListener('click',showSelected,printLion, e => {
+                                                                                                                        ;
+ });
+ function printLion(){ console.log("Lion")};
+
+const Leopard = document.querySelector(".big-five-button2");
+ Leopard.addEventListener('click',showSelected,printLeopard, e => {
+    
+});
+ function printLeopard() {console.log("Leopard")};
+
+const Elephant = document.querySelector(".big-five-button3");
+ Elephant.addEventListener('click',showSelected,printElephant, e => {
+ 
+});
+function printElephant(){console.log("Elephant")};
+
+const Rhino = document.querySelector(".big-five-button4");
+ Rhino.addEventListener('click',showSelected,printRhino, e => {
+    
+});
+function printRhino(){console.log("Rhino")};
+
+const Buffalo = document.querySelector(".big-five-button5");
+ Buffalo.addEventListener('click',showSelected,printBuffalo, e => {
+   
+});
+function printBuffalo(){console.log("Buffalo")};
 
 
-const Lion = document.querySelector("big-five-button1",Lion);
-Lion.addEventListener('click',click,showSelected);
+//click registratie en toevoegen list element
+function showSelected () {
+   
+    if(this.click){ 
+       
+        const ListElement = document.createElement("li");
+        ListElement
+        ListElement.classList.add("spotted-animals-list-item");
+        ListElement
+         const List = document.getElementById("spotted-animals-list");
+         List
+         
+         List.appendChild(ListElement);
+        ListElement.innerHTML = (this.value); 
+       console.log(this.value);
+};  };
+//verwijderen van eerste list item
+  const removeChild = document.getElementById("remove-first-item-button");
+  removeChild.addEventListener('click',removeElement);
 
-const Leopard = document.querySelector("big-five-button2",Leopard);
-Leopard.addEventListener('click',click,showSelected);
-
-const Elephant = document.querySelector("big-five-button3",Elephant);
-Elephant.addEventListener('click',click,showSelected);
-
-const Rhino = document.querySelector("big-five-button4",Rhino);
-Rhino.addEventListener('click',click,showSelected);
-
-const Buffalo = document.querySelector("big-five-button5",Buffalo);
-Buffalo.addEventListener('click',click,showSelected);
-
-document.querySelectorAll("#spotted-animal-list");
- const spottedanimalslistitem = document.createElement("li");
-spottedanimalslistitem
-
-const listElement = document.add("#spotted-animals-list");
-listElement
-
-const list = document.querySelectorAll("#spotted-animal-list");
-
-
-for(const button of animalButtons){
-    button.addEventListener('change', showSelected);
-    ul.addEventListener('click',click, ()=>{ul.style.background = "#blue" });
-} 
-function showSelected(e) {
-    console.log(e);
+  function removeElement() {
     if(this.click){
-document.querySelector("#spotted-animals-list").innerHTML = `You selected ${this.click}`;
-list.appendChild(spottedanimalslistitem);
-spottedanimalslistitem.appendChild(listElement);
-listElement.appendChild(li.innerHTML = this.click);
-}
-    }
-};
+    
+    const parent = document.getElementById("spotted-animals-list");
+    parent
+    const child = parent.getElementsByClassName("spotted-animals-list-item")[0];
+    child
+    parent.removeChild(child);
+    
+    console.log();
+  }};
+ //verwijderen van alle list items 
+  const removeAllAnimals = document.getElementById("remove-all-button");
+  removeAllAnimals.addEventListener('click',removeAll);
 
-console.log(animalButtons);
- console.log("big-five-button1","big-five-button2","big-five-button3","big-five-button4","big-five-button5");
+  function removeAll() {
+    if(this.click){
+       const parent =  document.getElementById('spotted-animals-list');
+      while(parent.firstChild){parent.removeChild(parent.firstChild);} 
+    }
+ };
